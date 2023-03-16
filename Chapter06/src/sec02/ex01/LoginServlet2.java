bc7809cdb0d53fb3bd0706d22219f0c621a73d1b
Hwasil <127382144+Hwasil@users.noreply.github.com>
2023-03-16 16:17:38 +0900

    Add files via upload

---
+++ Chapter06/src/sec02/ex01/LoginServlet2.java
@@ -0,0 +1,62 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet2
+ */
+@WebServlet("/Login2")
+public class LoginServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8"); // 데이터 타입 정해주기
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		// [html 안에 각 데이터를 출력한 다는 것을 보여줌] 아이디와 비번 가져온 것을 화면에 출력
+		String data = "<html>";
+		data += "<body>";
+		data += "ID : " + user_id;
+		data += "<br>";
+		data += "PW : " + user_pw; 
+		data += "</body>"; // 짝 맞춰주기
+		data += "</html>"; 
+		
+		// 내용 출ㄺ
+		out.print(data);
+	}
+
+}
---
+++ Chapter06/src/sec04/ex01/LoginServlet5.java
@@ -0,0 +1,81 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet4
+ */
+@WebServlet("/login5")
+public class LoginServlet5 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doGet 메서드 호출");
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doPost 메서드 호출");
+		doHandle(request, response);
+	}
+	
+	@SuppressWarnings("unused")
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doHandle 메서드 호출");
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8"); // 데이터 타입 정해주기
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		String user_addr = request.getParameter("user_address");
+		
+		// [html 안에 각 데이터를 출력한 다는 것을 보여줌] 아이디와 비번 가져온 것을 화면에 출력
+		String data = "<html>";
+		data += "<body>";
+		data += "ID : " + user_id;
+		data += "<br>";
+		data += "PW : " + user_pw; 
+		data += "<br>";
+		data += "Address : " + user_addr; 
+		data += "</body>"; // 짝 맞춰주기
+		data += "</html>"; 
+		
+		out.print(data);
+	}
+
+}
